0b3152f1bf1f62a7b2ae87629a02f7a37c629ccc
Colin Toomey <86315504+crtoomey@users.noreply.github.com>
2025-11-24 13:56:53 -0500

    Modified ball.gd Made a new function to handle the ball's movement because before it was only moving right unless it collided with another object. Added a change to the ball's Y direction when it collides with the paddle. Still need to randomize the speed so it doesn't always change direction at the same angle. Added logic to change Y direction when the ball hits the top and bottom borders, which just changes the Y direction by multiplying it by -1.

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -12,7 +12,7 @@ func _physics_process(delta: float) -> void:
 	
 
 	# this will start the ball going to the left side of the screen
-	position.x += directionX * speed * delta
+	move_ball(delta)
 	
 	# this returns some info about the object the ball collides with but I don't understand it fully yet
 	var collisionInfo = move_and_collide(velocity * delta)
@@ -20,23 +20,33 @@ func _physics_process(delta: float) -> void:
 	# check to see if a collision occurred
 	if collisionInfo:
 		
-		var normal = collisionInfo.get_normal()
-		print(normal)
-		
 		# get the object the ball collided with in Object form
 		var collider = collisionInfo.get_collider()
 		# get the name of the object
 		var colliderName = collider.name
 		print(colliderName)
+		
+		
+		
 		# if the object is named PlayerPaddle, the ball moves to the right
 		if colliderName == "PlayerPaddle":
+
+			directionY = -1
+			position.y += directionY * speed * delta
 			directionX = 1
 			position.x += directionX * speed * delta
+		# if the ball hits one of the borders, change y direction to the opposite direction
+		elif colliderName == "Border":
+			directionY = directionY * -1
 		# if not, the ball moves to the left
 		else: 
 			directionX = -1
 			position.x += directionX * speed * delta
 
+
+func move_ball(delta):
+	position.x += directionX * speed * delta
+	position.y += directionY * speed * delta
 	
 	
 	
